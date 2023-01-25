aaab2e0dc0dccbd98cc35cd28c2ab9bf807a21b9
ctysingh <chaitanyasingh@gmail.com>
2023-01-25 08:29:19 -0500

    handle event from exec process

--- watcher-spawn.js
+++ watcher-spawn.js
@@ -4,15 +4,31 @@ const fs = require('fs');
 const { exec } = require('child_process');
 const { spawn } = require('child_process');
 //const ls = spawn('ls', ['-lh', '/usr']);
-const url = "https://www.youtube.com/";
-const cmdCommand = `start chrome /new-tab ${url}`;
-try{
-    exec(cmdCommand);
+
+const exceCmdType = process.argv[2];
+if(exceCmdType === 1) {
+    const url = "https://www.youtube.com/";
+    const cmdCommand = `start chrome /new-tab ${url}`;
+    try{
+        exec(cmdCommand);    
+    }
+    catch(error){
+        console.log(error);
+    }
+} else {
+    exec('ping google.com', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout:\n${stdout}`);
+    });
 }
-catch(error){
-    console.log(error);
-}
-//const fileName = process.argv[2];
+
 
 /*
 if(!fileName){
